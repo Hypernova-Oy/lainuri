d5a6b9707461d0c3dccf56f5ac289935e52e269f
Olli-Antti Kivilahti <olli-antti.kivilahti@hypernova.fi>
2020-02-15 08:11:48 +0200

    Ansible installer. Thermal printer installer fixes.

--- installer/thermal_printer/configure_printer_HS-K33.sh
+++ installer/thermal_printer/configure_printer_HS-K33.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo "1. Unarchiving the vendor CUPS driver"
-# Encrypting the vendor SDK, Docs, and Driver to prevent leaking confidential information to version control.
-#gpg --batch --no-tty --yes --passphrase-file ../../installer_secret_passphrase --encrypt -o HS-K33.tar.gz.gpg HS-K33.tar.gz
-gpg --batch --no-tty --yes --passphrase-file ../../installer_secret_passphrase --decrypt -o HS-K33.tar.gz HS-K33.tar.gz.gpg
-tar -xzf HS-K33.tar.gz
+echo "1. Make sure CUPS is installed"
+sudo apt install cups
 
 echo "2. Deploy the driver"
-sudo cp HS-K33/Drivers/Linux/filter/64/rastertopos /usr/lib/cups/filter/
-chmod a+x /usr/lib/cups/filter/rastertopos
-sudo cp HS-K33/Drivers/Linux/ppd/pos80.ppd /usr/lib/cups/driver/pos80.ppd
+sudo cp rastertopos /usr/lib/cups/filter/
+sudo chmod 755 /usr/lib/cups/filter/rastertopos
+sudo chown root:root /usr/lib/cups/filter/rastertopos
+sudo cp pos80.ppd /usr/lib/cups/driver/pos80.ppd
+sudo chmod 644 /usr/lib/cups/driver/pos80.ppd
+sudo chown root:root /usr/lib/cups/driver/pos80.ppd
 
 echo "3. Adding a system CUPS printer"
 PRINTER_URL=$(lpinfo -v | grep -P '^direct' | grep -Po 'usb://Unknown/Printer\?serial=(.+)$')
-lpadmin -p HS-K33 -P /usr/lib/cups/driver/pos80.ppd -u allow:all -E -v usb://Unknown/Printer?serial=002F00420238AB3900000ECC
+test -z $PRINTER_URL && \
+  echo "Couldn't find the printer with lpinfo! Is the printer plugged in directly to the Raspberry Pi and identified as a USB-device?" && \
+  exit 10
+
+lpadmin -p HS-K33 -P /usr/lib/cups/driver/pos80.ppd -u allow:all -E -v "$PRINTER_URL"
+
 # Configure as system default printer, with feed and half cut + buzzer
 lpoptions -p HS-K33 -o OptionCutPaperAfterPage=1 -o OptionBuzzingAfterPage=1 -o PageSize=72mmx100mm
 lpoptions -d HS-K33
@@ -25,3 +30,5 @@ if [ $? != 0 ]; then
   # TODO: message zabbix of failure notifications
   exit 10
 fi
+
+
