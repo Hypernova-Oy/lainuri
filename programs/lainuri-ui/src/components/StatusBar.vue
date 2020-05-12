f1ba6b4572011c97503ff60acd7f81dfc3b68d93
Olli-Antti Kivilahti <olli-antti.kivilahti@hypernova.fi>
2020-05-12 07:06:00 +0300

    lainuri-ui - StatusBar floats to left. Dynamic width.
    Less obstructive when only paper running out status.

--- programs/lainuri-ui/src/components/StatusBar.vue
+++ programs/lainuri-ui/src/components/StatusBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-snackbar
-    :value="show_statusbar"
-    color="error"
-    :timeout="0"
-    top right
+  <div
+    v-show="statusbar_width"
+    class="status-bar error v-snack__wrapper"
     @click="show_tooltips = !show_tooltips"
+    :style="{minWidth: statusbar_width + 'px'}"
   >
+  <div class="v-snack__content">
     <v-tooltip v-if="server_connected != Status.SUCCESS" bottom v-model="show_tooltips">
       <template v-slot:activator="{ on }">
         <v-icon v-on="on">mdi-server-off</v-icon>
@@ -24,7 +24,7 @@
       </template>
       {{t('StatusBar/Printer_paper_runout')}}
     </v-tooltip>
-    <v-tooltip v-else-if="status.thermal_printer_paper_status != Status.SUCCESS" left v-model="show_tooltips">
+    <v-tooltip v-else-if="status.thermal_printer_paper_status == Status.PENDING" left v-model="show_tooltips">
       <template v-slot:activator="{ on }">
         <v-icon color="yellow darken-2" v-on="on">mdi-paper-roll-outline</v-icon>
       </template>
@@ -42,14 +42,20 @@
       </template>
       {{t('StatusBar/Barcode_reader_off')}}
     </v-tooltip>
-    <v-tooltip v-if="status.ils_connection_status != Status.SUCCESS" left v-model="show_tooltips">
+    <v-tooltip v-if="status.ils_connection_status == Status.ERROR" left v-model="show_tooltips">
       <template v-slot:activator="{ on }">
-        <v-icon v-if="status.ils_connection_status == Status.ERROR"   color="error" v-on="on">mdi-server-network-off</v-icon>
-        <v-icon v-else-if="status.ils_connection_status == Status.PENDING" color="yellow darken-2" v-on="on">mdi-server-network-off</v-icon>
+        <v-icon v-on="on">mdi-server-network-off</v-icon>
       </template>
       {{t('StatusBar/ILS_connection_lost')}}
     </v-tooltip>
-  </v-snackbar>
+    <v-tooltip v-if="status.ils_connection_status == Status.PENDING" left v-model="show_tooltips">
+      <template v-slot:activator="{ on }">
+        <v-icon color="yellow darken-2" v-on="on">mdi-server-network-off</v-icon>
+      </template>
+      {{t('StatusBar/ILS_connection_lost')}}
+    </v-tooltip>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -92,16 +98,24 @@ export default {
     show_tooltips: false,
   }),
   computed: {
-    show_statusbar: function () {
+    statusbar_width: function () {
+      let width = 0;
       for (let key of Object.keys(this.$data.status)) {
-        if (this.$data.status[key] != Status.SUCCESS) return true;
+        if (this.$data.status[key] != Status.SUCCESS) width += 32;
       }
-      return false;
+      if (this.$data.server_connected != Status.SUCCESS) width += 32;
+      return width;
     },
   },
 }
 </script>
 
 <style scoped>
-
+.status-bar {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 99;
+  margin: 3px;
+}
 </style>
